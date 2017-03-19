3e806ced795eaca6e8c837c37a93a0d4afc54c38
Nosskirneh <andreas.karl.henriksson@gmail.com>
2017-03-19 10:39:24 +0100

    Updated settings pane

--- ssprefs/SpotifySwitchesPrefs.m
+++ ssprefs/SpotifySwitchesPrefs.m
@@ -47,3 +47,60 @@
 }
 
 @end
+
+
+@interface PSTableCell : UITableViewCell
+- (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier specifier:(PSSpecifier *)specifier;
+@end
+
+@interface SpotifySwitchesSettingsHeaderCell : PSTableCell
+@end
+
+@implementation SpotifySwitchesSettingsHeaderCell {
+    UILabel *_headerLabel;
+    UILabel *_subheaderLabel;
+}
+
+- (id)initWithSpecifier:(PSSpecifier *)specifier {
+    self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell" specifier:specifier];
+    if (self) {
+        UIFont *font = [UIFont fontWithName:@"HelveticaNeue-Thin" size:40];
+        
+        _headerLabel = [[UILabel alloc] init];
+        [_headerLabel setText:@"SpotifySwitches"];
+        [_headerLabel setTextColor:[UIColor blackColor]];
+        [_headerLabel setFont:font];
+        
+        _subheaderLabel = [[UILabel alloc] init];
+        [_subheaderLabel setText:@"by Andreas Henriksson"];
+        [_subheaderLabel setTextColor:[UIColor grayColor]];
+        [_subheaderLabel setFont:[font fontWithSize:17]];
+        
+        [self addSubview:_headerLabel];
+        [self addSubview:_subheaderLabel];
+    }
+    return self;
+}
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [_headerLabel sizeToFit];
+    [_subheaderLabel sizeToFit];
+    
+    CGRect frame = _headerLabel.frame;
+    frame.origin.y = 20;
+    frame.origin.x = self.frame.size.width / 2 - _headerLabel.frame.size.width / 2;
+    _headerLabel.frame = frame;
+    
+    frame.origin.y += _headerLabel.frame.size.height - 10;
+    frame.origin.x = self.frame.size.width / 2 - _subheaderLabel.frame.size.width / 2;
+    _subheaderLabel.frame = frame;
+}
+
+- (CGFloat)preferredHeightForWidth:(CGFloat)width {
+    // Return a custom cell height.
+    return 80;
+}
+
+@end
